5df953bb6c02426589b35ff674e9ed3239b4ffdb
Kairat Nagmetov <kairatan09@gmail.com>
2023-06-03 16:24:06 +0500

    Add HW-tasks.

--- Sem7/HW7/HW_7_2.py
+++ Sem7/HW7/HW_7_2.py
@@ -16,7 +16,7 @@ def print_operation_table(operation, num_rows, num_columns):
     for i in range(1 , num_rows + 1):
         list = []
         for j in range(1, num_columns + 1):
-            list.append(i * j)
+            list.append(f"{i * j}\t")
         print(*list)    
 
 print_operation_table(lambda x, y: x * y, int(input('row: ')), int(input('column: '))) 
